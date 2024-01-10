73363d7f28d659acd136a3c18b0c05dd2f8f47f3
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 12:57:17 +0200

    Add project files.

---
+++ 24.Abstract/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vehicle car1 = new Vehicle(); //this is not possible as parent class is abstract! to increase security
+            Corsa car2 = new Corsa();
+            Console.WriteLine(car2.wheels);
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Vehicle
+    {
+
+        public int wheels = 4;
+    }
+
+    class Corsa : Vehicle
+    { 
+        public int sparewheels = 1;
+    }
+    
+}
